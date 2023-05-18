2c615e122a13f2b8751de25bf7b2b876a17b063a
Sh1tters <justinbestworldwide@gmail.com>
2023-05-18 22:02:21 +0200

    Add `/usage` resource to `/api-key`

--- pkg/routes/api_key.go
+++ pkg/routes/api_key.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/Frier03/KeyAuth-API/pkg/controllers"
+	"github.com/Frier03/KeyAuth-API/pkg/middleware"
 )
 
 // Set up auth keys route
@@ -11,5 +12,6 @@ func SetupAPIKeyRoutes(r *gin.Engine) {
 	apiKeyRoutes := r.Group("/api-key")
 	{
 		apiKeyRoutes.GET("/generate", controllers.GenerateAPIKeyHandler)
+		apiKeyRoutes.GET("/usage", middleware.APIKeyValidationMiddleware(), controllers.APIKeyUsageHandler)
 	}
 }
