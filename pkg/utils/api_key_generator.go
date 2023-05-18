f56a499f769937a0a88b21997709b4ab51305f7c
Sh1tters <justinbestworldwide@gmail.com>
2023-05-18 18:52:07 +0200

    Create `GenerateAPIKey()` for DEBUGGING only!

---
+++ pkg/utils/api_key_generator.go
@@ -0,0 +1,12 @@
+package utils
+
+// Function to generate an authentication key
+func GenerateAPIKey() string {
+	// Generate a random authentication key or use a predefined key for testing/debugging purposes
+	// You can implement your logic here to generate or retrieve an authentication key
+
+	// For demonstration purposes, we'll use a static authentication key
+	API_KEY := "debug-api-key"
+
+	return API_KEY
+}
