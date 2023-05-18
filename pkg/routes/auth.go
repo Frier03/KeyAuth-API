98a866c797889cea7f0731f7ed06b3b846e51b87
Sh1tters <justinbestworldwide@gmail.com>
2023-05-18 18:22:45 +0200

    Integrated `ValidateModelMiddleware` to `/login` & `/register`

--- pkg/routes/auth.go
+++ pkg/routes/auth.go
@@ -1,28 +1,61 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
+	"github.com/Frier03/KeyAuth-API/pkg/middleware"
+	"github.com/Frier03/KeyAuth-API/pkg/models"
 )
 
-// Set up routes for authentication
+// SetupAuthRoutes sets up the authentication routes.
 func SetupAuthRoutes(r *gin.Engine) {
 	authRoutes := r.Group("/auth")
 	{
-		authRoutes.POST("/login", loginHandler)
-		authRoutes.POST("/register", registerHandler)
-		authRoutes.POST("/logout", logoutHandler)
+		authRoutes.POST("/login",
+			middleware.ValidateModelMiddleware(&models.LoginRequest{}), // Validate login request payload
+			loginHandler, // Expected resource logic
+		)
+
+		authRoutes.POST("/register",
+			middleware.ValidateModelMiddleware(&models.RegisterRequest{}), // Validate register request payload
+			registerHandler, // Expected resource logic
+		)
+
+		authRoutes.POST("/logout",
+			logoutHandler, // Expected resource logic
+		)
 	}
 }
 
 func loginHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
+	// Retrieve the login request model from the context
+	req, _ := c.Get("model")
+
+	// Try assert the request model to *models.LoginRequest
+	loginReq, _ := req.(*models.LoginRequest)
+
+	// Access the username and password from the request
+	_, _ = loginReq.Username, loginReq.Username // Intentionally ignoring these variables
+
+	c.JSON(http.StatusOK, gin.H{
 		"message": "Login successful",
 	})
 }
 
 func registerHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Registration successful",
+	// Retrieve the login request model from the context
+	req, _ := c.Get("model")
+
+	// Try assert the request model to *models.LoginRequest
+	registerReq, _ := req.(*models.RegisterRequest)
+
+	// Access the username and password from the request
+	_, _ = registerReq.Username, registerReq.Username // Intentionally ignoring these variables
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Logout successful",
 	})
 }
 
