fd344f625b236266c54a360d1b21caa69c29208a
Sh1tters <justinbestworldwide@gmail.com>
2023-05-18 22:03:09 +0200

    Add **handler** for `/api-key/usage`

--- pkg/controllers/api_key_controller.go
+++ pkg/controllers/api_key_controller.go
@@ -10,10 +10,20 @@ import (
 // Handler for generating an authentication key
 func GenerateAPIKeyHandler(c *gin.Context) {
 	// Generate an authentication key
-	API_KEY := utils.GenerateAPIKey()
+	apiKey := utils.GenerateAPIKey()
 
 	// Return the authentication key in the response
 	c.JSON(http.StatusOK, gin.H{
-		"API_KEY": API_KEY,
+		"API_KEY": apiKey,
+	})
+}
+
+// Handler for the /api-key/usage endpoint
+func APIKeyUsageHandler(c *gin.Context) {
+	// Your logic to check the API key usage here
+
+	// Assuming you want to return a JSON response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "API key usage endpoint",
 	})
 }
