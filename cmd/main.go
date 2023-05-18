21ae2267e49a6511a4fc1a6d26c44258ab0248fd
Sh1tters <justinbestworldwide@gmail.com>
2023-05-18 18:21:37 +0200

    Update `SetTrustedProxies` for `IPv6`, `IPv4` & `Hostname`

--- cmd/main.go
+++ cmd/main.go
@@ -8,8 +8,22 @@ import (
 func main() {
 	r := gin.Default()
 
+	// Set ForwardedByClientIP to true to trust the X-Forwarded-For header
+	// This ensures that the correct client IP is used in the request context
+	r.ForwardedByClientIP = true
+
+	// Set trusted proxies to handle X-Forwarded-For headers
+	r.SetTrustedProxies([]string{
+		"127.0.0.1", // IPv4 address for localhost
+		"::1",       // IPv6 address for localhost
+		"localhost", // Hostname for localhost
+	})
+
+	// Running GIN in "debug" mode. Switch to "release" mode in production.
+	gin.SetMode(gin.DebugMode) //gin.ReleaseMode
+
 	// Set up authentication routes
 	routes.SetupAuthRoutes(r)
 
-	r.Run() // Start the server
+	r.Run(":8080") // Start the server
 }
