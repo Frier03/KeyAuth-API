0c62189b5246c009ea6bd2ea2a657e43791adb93
Sh1tters <justinbestworldwide@gmail.com>
2023-05-19 20:39:12 +0200

    All generator methods put into one file

--- pkg/utils/generators.go
+++ pkg/utils/generators.go
@@ -11,19 +11,27 @@ import (
 // Function to generate a modified API key
 func GenerateAPIKey() string {
 	// Generate a UUID
-	id := uuid.New()
-
-	// Convert the UUID to a string
-	uuidStr := id.String()
+	uuid := GenerateUUID()
 
 	// Hash the UUID using MD5
-	hash := md5.Sum([]byte(uuidStr))
+	hash := md5.Sum([]byte(uuid))
 
 	// Convert the hash to a hexadecimal string
 	hashStr := hex.EncodeToString(hash[:])
 
 	// Join the UUID and hash strings
-	apiKey := strings.Join([]string{uuidStr, hashStr}, "-")
+	apiKey := strings.Join([]string{uuid, hashStr}, "-")
 
 	return apiKey
+}
+
+// Function to generate UUID
+func GenerateUUID() string {
+	// Generate new UUID
+	id := uuid.New()
+
+	// Convert the UUID to a string
+	uuid := id.String()
+
+	return uuid
 }
