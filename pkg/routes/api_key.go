4214dda6fd7ad50dbf66979d7ad307c6248df4fc
Sh1tters <justinbestworldwide@gmail.com>
2023-05-20 22:21:55 +0200

    Implemented `dependencies` to `SetupAPIKeyRoutes`

--- pkg/routes/api_key.go
+++ pkg/routes/api_key.go
@@ -4,14 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/Frier03/KeyAuth-API/pkg/controllers"
+	"github.com/Frier03/KeyAuth-API/pkg/dependencies"
 	"github.com/Frier03/KeyAuth-API/pkg/middleware"
+	"github.com/Frier03/KeyAuth-API/pkg/models"
 )
 
 // Set up auth keys route
-func SetupAPIKeyRoutes(r *gin.Engine) {
+func SetupAPIKeyRoutes(r *gin.Engine, deps dependencies.Dependencies) {
 	apiKeyRoutes := r.Group("/api-key")
 	{
-		apiKeyRoutes.GET("/generate", controllers.GenerateAPIKeyHandler)
-		apiKeyRoutes.GET("/usage", middleware.APIKeyValidationMiddleware(), controllers.APIKeyUsageHandler)
+		apiKeyRoutes.GET("/generate",
+			middleware.ValidateModelMiddleware(&models.APIKeyGenerateRequest{}),
+			func(c *gin.Context) {
+				controllers.GenerateAPIKeyHandler(c, deps)
+			})
+
+		apiKeyRoutes.GET("/usage",
+			middleware.APIKeyValidationMiddleware(),
+			controllers.APIKeyUsageHandler,
+		)
 	}
 }
