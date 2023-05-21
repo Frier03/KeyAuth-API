1c133e890159de2a8c9e5c49e36b61de1289496c
Sh1tters <justinbestworldwide@gmail.com>
2023-05-21 09:10:31 +0200

    Temp removed `isValidAPIKey`

--- pkg/middleware/api_key_middleware.go
+++ pkg/middleware/api_key_middleware.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,18 +10,8 @@ func APIKeyValidationMiddleware() gin.HandlerFunc {
 
 		apiKey := c.GetHeader("X-Api-Key")
 		// Check if the API key is valid
-		if !isValidAPIKey(apiKey) {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "Invalid API key",
-			})
-			return
-		}
 
+		c.Set("x-api-key", apiKey)
 		c.Next()
 	}
-}
-
-// isValidAPIKey is a function to validate the API key
-func isValidAPIKey(apiKey string) bool {
-	return apiKey == "a1f44782-6499-4d1e-948d-1ab37ad23b82-9a79bc5f585b0d7002a3400943585372"
 }
