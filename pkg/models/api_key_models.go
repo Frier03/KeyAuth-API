0c86223aaa86fa0b533f49c903a4d1022e112cc3
Sh1tters <justinbestworldwide@gmail.com>
2023-05-21 09:10:17 +0200

    Add `SubjectID`. `Roles`, `Limit`, `LastUsed`,`Active` and removed `APIKey`

--- pkg/models/api_key_models.go
+++ pkg/models/api_key_models.go
@@ -12,8 +12,12 @@ type APIKeyGenerateRequest struct {
 // APIKey struct that represents the API key table in the database
 type APIKey struct {
 	ID        string    `json:"id"`
-	APIKey    string    `json:"api_key"`
+	SubjectID string    `json:"subject_id"`
+	Roles     []string  `json:"roles"`
 	Usage     int       `json:"usage"`
+	Limit     int       `json:"limit"`
 	CreatedAt time.Time `json:"created_at"`
 	ExpiresAt time.Time `json:"expires_at"`
+	LastUsed  time.Time `json:"last_used"`
+	Active    bool      `json:"active"`
 }
