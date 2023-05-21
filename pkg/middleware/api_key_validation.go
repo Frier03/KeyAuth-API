02f1f19cadc5d4ff7afec956ff691f3469e133f7
Sh1tters <justinbestworldwide@gmail.com>
2023-05-21 12:40:43 +0200

    Updates to return retrieve API key data. Will improve performance, less database queries needed

---
+++ pkg/middleware/api_key_validation.go
@@ -0,0 +1,56 @@
+package middleware
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Frier03/KeyAuth-API/pkg/models"
+	"github.com/Frier03/KeyAuth-API/pkg/services"
+	"github.com/gin-gonic/gin"
+)
+
+// APIKeyValidationMiddleware is a middleware to validate the API key
+func APIKeyValidationMiddleware(badgerService *services.BadgerService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		apiKey := c.GetHeader("X-Api-Key")
+
+		// Lookup the API key in the database using badgerService
+		value, err := badgerService.Get([]byte(apiKey))
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+				"error": "Invalid API key",
+			})
+			return
+		}
+
+		// Is the API Key not in the database?
+		if value == nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error": "API key Not Found",
+			})
+			return
+		}
+
+		// Unmarshal the API key value into an APIKey object
+		var apiKeyModel models.APIKey
+		err = json.Unmarshal(value, &apiKeyModel)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "API key has invalid format?",
+			})
+			return
+		}
+
+		// Is the API Key not active?
+		if !apiKeyModel.Active {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Inactive API key",
+			})
+			return
+		}
+
+		// Set custom header for the api key model retrieved
+		c.Set("api-key-model", apiKeyModel)
+		c.Next()
+	}
+}
