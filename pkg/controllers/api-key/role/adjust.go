7ea7dba1b24091d694783a1efcb5282972e41d50
Sh1tters <justinbestworldwide@gmail.com>
2023-05-21 18:59:47 +0200

    New Feature: `Role Access Control`. See description for more details.
    Feature includes:
    - Role access based routes
    - Adjustable role permissions - supports only 0-2
    
    - Lots of bug fixes

---
+++ pkg/controllers/api-key/role/adjust.go
@@ -0,0 +1,52 @@
+package role
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/Frier03/KeyAuth-API/pkg/models"
+	"github.com/Frier03/KeyAuth-API/pkg/services"
+	"github.com/gin-gonic/gin"
+)
+
+func Adjust(c *gin.Context, badgerService *services.BadgerService) {
+	apiKey := c.GetHeader("X-Api-Key")
+	adjust, ok := c.GetQuery("permission_level")
+	apiKeyData, _ := c.Get("api-key-model")
+
+	// Apply model to apiKey
+	apiKeyModel, _ := apiKeyData.(models.APIKey)
+	fmt.Println(adjust, ok)
+	// Validate adjust value
+	level, err := strconv.Atoi(adjust)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid adjust value",
+		})
+		return
+	}
+
+	// Check if level is within the allowed range
+	if level < int(models.Default) || level > int(models.Admin) {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Adjust value is out of range",
+		})
+		return
+	}
+
+	apiKeyModel.PermissionLevel = level
+
+	// Update the API key in the database
+	err = badgerService.PutAPIKey([]byte(apiKey), &apiKeyModel)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to update API key",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
+}
