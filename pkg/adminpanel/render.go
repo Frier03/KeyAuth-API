37482be34b58a77f830c107a484cc955a86e40f1
Sh1tters <justinbestworldwide@gmail.com>
2023-05-23 22:11:32 +0200

    `RenderDashPage` now renders data on the panel

--- pkg/adminpanel/render.go
+++ pkg/adminpanel/render.go
@@ -1,13 +1,48 @@
 package adminpanel
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/Frier03/KeyAuth-API/pkg/models"
+	"github.com/Frier03/KeyAuth-API/pkg/services"
 	"github.com/gin-gonic/gin"
 )
 
-func RenderIndexPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", nil)
+func RenderDashboardPage(c *gin.Context, badgerService *services.BadgerService) {
+	apiKeyData, err := badgerService.GetAllData()
+	if err != nil {
+		// Handle error
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	// Convert the map data to models.APIKey struct
+	var apiKeys []models.APIKey
+
+	for _, data := range apiKeyData {
+		apiKeyBytes, err := json.Marshal(data)
+		if err != nil {
+			// Handle error
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		var apiKey models.APIKey
+		err = json.Unmarshal(apiKeyBytes, &apiKey)
+		if err != nil {
+			// Handle error
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		apiKeys = append(apiKeys, apiKey)
+	}
+
+	// Render the dashboard.html template with the API key data
+	c.HTML(http.StatusOK, "dashboard.html", gin.H{
+		"APIKeys": apiKeys,
+	})
 }
 
 func RenderLoginPage(c *gin.Context) {
